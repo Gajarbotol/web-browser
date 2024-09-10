885bdddbb8600406b18a598995d0500ba620e070
Gajarbotol <123944033+Gajarbotol@users.noreply.github.com>
2024-09-10 19:21:20 +0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const axios = require('axios');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/fetch', async (req, res) => {
+  const url = req.query.url;
+  if (!url) {
+    return res.status(400).send('URL is required');
+  }
+
+  try {
+    const response = await axios.get(url);
+    res.send(response.data);
+  } catch (error) {
+    res.status(500).send('Error fetching the URL');
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
